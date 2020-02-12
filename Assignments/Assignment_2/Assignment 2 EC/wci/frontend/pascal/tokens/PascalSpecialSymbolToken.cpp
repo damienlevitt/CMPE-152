723410c5f7657e3fa834d114e9d38e22f23b3dc0
damienlevitt <damien.levitt@sjsu.edu>
2020-02-12 15:47:45 -0800

    added the files for the extra credit

---
+++ Assignments/Assignment_2/Assignment 2 EC/wci/frontend/pascal/tokens/PascalSpecialSymbolToken.cpp
@@ -0,0 +1,135 @@
+/**
+ * <h1>PascalSpecialSymbolToken</h1>
+ *
+ * <p> Pascal special symbol tokens.</p>
+ *
+ * <p>Copyright (c) 2017 by Ronald Mak</p>
+ * <p>For instructional purposes only.  No warranties.</p>
+ */
+#include <string>
+#include "PascalSpecialSymbolToken.h"
+#include "../PascalError.h"
+
+namespace wci { namespace frontend { namespace pascal { namespace tokens {
+
+using namespace std;
+using namespace wci::frontend;
+using namespace wci::frontend::pascal;
+
+void PascalSpecialSymbolToken::extract() throw (string)
+{
+    char current_ch = current_char();
+    bool good_symbol = true;
+    char eq = '=';
+    int counter = 0;
+    text = current_ch;
+
+    switch (current_ch)
+    {
+        // Single-character special symbols.
+        case '+':  case '-':  case '*':  case '/':  case ',':
+        case ';':  case '\'':  case '(':  case ')':
+        case '[':  case ']':  case '{':  case '}':  case '^':
+        	//case '=':
+        {
+            next_char();  // consume character
+            break;
+        }
+
+        // : or :=
+        case ':':
+        {
+            current_ch = next_char();  // consume ':';
+
+            if (current_ch == '=')
+            {
+                text += current_ch;
+                next_char();  // consume '='
+            }
+
+            break;
+        }
+
+        // < or <= or <>
+        case '<':
+        {
+            current_ch = next_char();  // consume '<';
+
+            if (current_ch == '=')
+            {
+                text += current_ch;
+                next_char();  // consume '='
+            }
+            else if (current_ch == '>')
+            {
+                text += current_ch;
+                next_char();  // consume '>'
+            }
+
+            break;
+        }
+
+        // > or >=
+        case '>':
+        {
+            current_ch = next_char();  // consume '>';
+
+            if (current_ch == '=')
+            {
+                text += current_ch;
+                next_char();  // consume '='
+            }
+
+            break;
+        }
+
+        // . or ..
+        case '.':
+        {
+            current_ch = next_char();  // consume '.';
+
+            if (current_ch == '.')
+            {
+                text += current_ch;
+                next_char();  // consume '.'
+            }
+
+            break;
+        }
+        //fix line 99-110
+        case '='://first
+        {
+        eq = next_char();
+        if(eq == '='){
+        	eq = peek_char();
+        	if(eq == '>'){
+        		text += current_ch;//=
+        		//text += current_ch;//=
+        		text += eq;//>
+        		next_char();//Consume
+        		next_char();
+        	}
+        	//next_char();
+        }
+
+        	break;
+
+        }
+
+
+        default:
+        {
+            next_char();  // consume bad character
+            type = (TokenType) (PT_ERROR);
+            value = (int) INVALID_CHARACTER;
+            good_symbol = false;
+        }
+    }
+
+    // Set the type if it wasn't an error.
+    if (good_symbol) {
+        type = (TokenType) (PascalToken::SPECIAL_SYMBOLS[text]);
+    }
+}
+
+}}}}  // namespace wci::frontend::pascal::tokens
